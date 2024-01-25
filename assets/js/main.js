9f6f9c840a34c332750adfcb80c74df543b14c33
Gustavo Silva <guh_sz@outlook.com>
2024-01-25 19:46:14 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,54 @@
-function criaCalculdora () {
-    return {
-            display: document.querySelector('.display'),
-            btnClear: document.querySelector('.btn-clear'),
+function Calculadora () {
+    this.display= document.querySelector(".display");
 
-            clearDisplay() {
-                this.display.value = "";
-
-            },
-
-            apagaUm() {
-                this.display.value=this.display.value.slice(0, -1);
-            },
-
-            inicia() {
-                this.cliqueBotoes();
-                this.pressionaEnter();
-            },
-
-            pressionaEnter() {
-                this.display.addEventListener('keyup', e=> {
-                    if(e.keyCode===13) {
-                        this.realizaConta();
-                    }
-                });
-            },
-
-            realizaConta() {
-                let conta = this.display.value;
-
-                try {
-                    conta = eval(conta);
-
-                    if (!conta) {
-                        alert('Erro de Conta');
-                        return;
-                    }
-
-                    this.display.value = String(conta);
-                } catch (e){
-                    alert("Erro de Conta");
-                    return;
-                }
-            },
-
-            
-        cliqueBotoes() {
-            document.addEventListener('click', e => {
-                const el = e.target;
-
-                if(el.classList.contains("btn-num")) {
-                    this.btnParaDisplay(el.innerText);
-                }
-
-                if(el.classList.contains('btn-clear')) {
-                    this.clearDisplay();
-                }
-
-                if(el.classList.contains('btn-del')) {
-                    this.apagaUm();
-                }
-                if(el.classList.contains('btn-eq')) {
-                    this.realizaConta();
-                }
-            });
-        },
-        btnParaDisplay(valor){
-            this.display.value +=valor;
-        }
+    this.inicia= ()=> {
+        this.capturaCliques();
+        this.capturaEnter();
     };
+
+    this.capturaEnter = () => {
+        document.addEventListener('keypress', e => {
+            if(e.keyCode === 13) {
+                this.realizaConta();
+            }
+        });
+    };
+
+    this.capturaCliques = () => {
+        document.addEventListener("click", event =>{
+            const el = event.target;
+            if(el.classList.contains("btn-num")) this.addNumDisplay(el);
+            if(el.classList.contains("btn-clear")) this.clear();
+            if(el.classList.contains("btn-del")) this.del(el);
+            if(el.classList.contains("btn-eq")) this.realizaConta(el);
+        });
+    };
+
+    this.addNumDisplay=(el)=>{ this.display.value += el.innerText;
+    this.display.focus();
+    };
+
+    this.clear = ()=> this.display.value='';
+
+    this.del= () => this.display.value=this.display.value.slice(0, -1);
+
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+            if(!conta) {
+                alert("Conta inválida!");
+                return;
+            }
+            this.display.value = conta;
+            } catch (e) {
+                alert('Erro!');
+                return;
+                }
+        
+    }
+
+    
 }
 
-const calculadora = criaCalculdora();
+const calculadora = new Calculadora();
 calculadora.inicia();
